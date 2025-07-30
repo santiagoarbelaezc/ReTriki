7b6d1f6ea646ab649dcdb10fcddd99c5a5077517
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2025-07-30 18:36:10 -0500

    nuevos cambios

---
+++ src/app/pages/retriki/retriki.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-retriki',
+  standalone: true,
+  imports: [],
+  templateUrl: './retriki.component.html',
+  styleUrl: './retriki.component.css'
+})
+export class RetrikiComponent {
+
+}
